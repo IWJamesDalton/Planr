70284cbdf168df929dc28969a77c8b163bed4a47
James <james.dalton@infinityworks.com>
2019-01-01 21:44:38 +0000

    Added loading spinner

--- www/pages/daily.vue
+++ www/pages/daily.vue
@@ -1,99 +1,109 @@
 <template>
   <v-layout row>
+
     <v-flex 
-      v-if="days" 
       xs12 
-      sm10>
+      sm12>
       <v-date-picker
         v-model="dateString"
         full-width
         landscape
         color="cyan lighten-1"
       />
-      <v-card 
-        v-for="(day, index) in days" 
-        :key="index">
-        <v-list two-line>
-          <template v-for="(task, index) in day.tasks" >
-            <div :key="index">
-              <v-subheader 
-                v-if="index == 0">
-                Most Important Task Of The Day
-              </v-subheader>
-              <v-subheader 
-                v-else-if="index == 1">
-                Secondary Tasks of Importance
-              </v-subheader>
-              <v-subheader 
-                v-else-if="index == 3">
-                Additional Tasks
-              </v-subheader>
-              <v-divider 
-                v-if="index > 0" 
-                :inset="true"/>
-              <v-list-tile>
-                <v-layout>
-                  <v-flex 
-                    xs12 
-                    sm10>
-                    <v-text-field 
-                      v-model="task.name" 
-                      label="Task"/>
-                  </v-flex>
-                  <v-flex 
-                    xs12 
-                    sm2 
-                    d-flex>
-                    <v-select
-                      v-model="task.pomodoro"
-                      :items="priorities"
-                      label="Pomodoro"
-                    />
-                  </v-flex>
-                </v-layout>
-              </v-list-tile>
-            </div>
-          </template>
-        </v-list>
-        <v-flex 
-          xs12 
-          px-3>
-          <v-textarea
-            v-model="day.notes"
-            label="Notes"
-          />
-          <v-subheader px-0>Productivity Score</v-subheader>
-          <v-slider
-            v-model="day.productivityScore"
-            :max="10"
-            :min="0"
-            color="cyan" 
-            thumb-label="always"
-            step="1"
-            ticks
-          />
-        </v-flex>
-        <v-btn 
-          :disabled="saving"
-          color="success"
-          px-3
-          @click="update">Update</v-btn>
-    </v-card></v-flex>
-    <v-snackbar
-      v-model="snackbar"
-      :right="true"
-      :timeout="5000"
-      :top="true"
-    >
-      {{ snackText }}
-      <v-btn
-        color="pink"
-        flat
-        @click="snackbar = false"
+      <v-progress-circular
+        v-if="!days"
+        :size="70"
+        :width="7"
+        color="cyan"
+        indeterminate
+      />
+      <div v-if="days" >
+        <v-card 
+          v-for="(day, index) in days" 
+          :key="index">
+          <v-list two-line>
+            <template v-for="(task, index) in day.tasks" >
+              <div :key="index">
+                <v-subheader 
+                  v-if="index == 0">
+                  Most Important Task Of The Day
+                </v-subheader>
+                <v-subheader 
+                  v-else-if="index == 1">
+                  Secondary Tasks of Importance
+                </v-subheader>
+                <v-subheader 
+                  v-else-if="index == 3">
+                  Additional Tasks
+                </v-subheader>
+                <v-divider 
+                  v-if="index > 0" 
+                  :inset="true"/>
+                <v-list-tile>
+                  <v-layout>
+                    <v-flex 
+                      xs12 
+                      sm10>
+                      <v-text-field 
+                        v-model="task.name" 
+                        label="Task"/>
+                    </v-flex>
+                    <v-flex 
+                      xs12 
+                      sm2 
+                      d-flex>
+                      <v-select
+                        v-model="task.pomodoro"
+                        :items="priorities"
+                        label="Pomodoro"
+                      />
+                    </v-flex>
+                  </v-layout>
+                </v-list-tile>
+              </div>
+            </template>
+          </v-list>
+          <v-flex 
+            xs12 
+            px-3>
+            <v-textarea
+              v-model="day.notes"
+              label="Notes"
+            />
+            <v-subheader px-0>Productivity Score</v-subheader>
+            <v-slider
+              v-model="day.productivityScore"
+              :max="10"
+              :min="0"
+              color="cyan" 
+              thumb-label="always"
+              step="1"
+              ticks
+            />
+          </v-flex>
+          <v-btn 
+            :disabled="saving"
+            color="success"
+            px-3
+            @click="update">Update</v-btn>
+        </v-card>
+      </div>
+      <v-snackbar
+        v-model="snackbar"
+        :right="true"
+        :timeout="5000"
+        :top="true"
       >
-        Close
-      </v-btn>
-    </v-snackbar>
+        {{ snackText }}
+        <v-btn
+          color="pink"
+          flat
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </v-snackbar>
+    </v-flex>
   </v-layout>
 </template>
 <script>
@@ -115,20 +125,15 @@ export default {
       },
       priorities: [0, 1, 2, 3, 4],
       apiBaseUrl: '',
-      blobBaseUrl: '',
       functionKey: '',
       api: {}
     }
     if (process.browser) {
       data.auth = {
         enabled: window.authEnabled,
-        loginUrl: window.auth.loginUrl,
-        token: window.auth.token,
-        logout: window.auth.logout,
-        username: null
+        token: window.auth.token
       }
       data.apiBaseUrl = window.apiBaseUrl
-      data.blobBaseUrl = window.blobBaseUrl
       data.functionKey = window.functionKey
     }
     return data
@@ -149,6 +154,7 @@ export default {
   watch: {
     dateString(dateString) {
       const date = new Date(dateString)
+      this.days = null
       this.initialized = false
       this.api
         .getDay(date)
@@ -173,6 +179,7 @@ export default {
     date(date) {
       this.dateString = date.toISOString().substr(0, 10)
       this.initialized = false
+      this.days = null
       this.api
         .getDay(date)
         .then(day => {
@@ -198,6 +205,7 @@ export default {
     if (!this.auth.enabled || this.loggedIn) {
       this.api = new Api(this.apiBaseUrl, this.functionKey, this.auth.token)
       this.initialized = false
+      this.days = null
       this.api
         .getDay(this.date)
         .then(day => {
